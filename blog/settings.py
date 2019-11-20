2347dfe8d8c1ffc8dbc6324c2660233d1880e51e
Ubuntu <ubuntu@ip-172-31-25-16.ec2.internal>
2019-11-20 20:15:27 +0000

    Added Herolu host to allowed host

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['72e47973215047b2bd4114b0d15e50de.vfs.cloud9.us-east-1.amazonaws.com', 'blogsabout.herokuapp.com/']
+ALLOWED_HOSTS = ['72e47973215047b2bd4114b0d15e50de.vfs.cloud9.us-east-1.amazonaws.com', 'blogsabout.herokuapp.com']
 
 
 # Application definition
