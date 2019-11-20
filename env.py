2ab3e48da8838d996047d10225abd025c0be6910
Ubuntu <ubuntu@ip-172-31-25-16.ec2.internal>
2019-11-20 20:11:26 +0000

    Removed env.py file

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "'(@5oo8vxc)vp*=xho3w9!)9!&d$w+e%a(g(ut!%*n48v7e(vg-'")
-
-os.environ.setdefault("DATABASE_URL", "postgres://mziqfuyaoldajz:a3372de7dcf2c3dd7d5eddcfdb20b6ca598462d97dcf70b0f3de2edb38dbcb8d@ec2-54-246-84-100.eu-west-1.compute.amazonaws.com:5432/d8a7s8qdlnqdkm")
